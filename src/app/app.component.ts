ee9abacbab62d5a88fbcc632255c9ecda19742b5
SurajK221b <suraj.khodade7@gmail.com>
2025-06-11 21:32:03 +0530

    Google analytics added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { CommonModule } from '@angular/common';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     HeaderComponent,
     FooterComponent,
     ContentComponent,
@@ -15,4 +20,13 @@ import { ContentComponent } from './content/content.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-53VB0B6L3M', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
 }
